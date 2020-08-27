69093379f4e094eaf15dee8d583f1358dabeb0f9
Elieder JW <eliederjw@gmail.com>
2020-08-27 17:47:38 -0300

    Lesson 7: Better Cubedude Movement. motion.length(). look_at().

--- Players/Player.gd
+++ Players/Player.gd
@@ -9,6 +9,8 @@ const UP = Vector3(0,1,0)
 
 func _physics_process(delta):
 	move()
+	animate()
+	face_forward()
 	
 func move():
 	
@@ -18,6 +20,14 @@ func move():
 	motion = Vector3(x,0,z)
 	
 	move_and_slide(motion.normalized() * SPEED, UP)
-	
 
+func animate():
+	if motion.length() > 0:
+		$AnimationPlayer.play("Arms Cross Walk")
+	else:
+		$AnimationPlayer.stop()
+
+func face_forward():
+	if not motion.x == 0 or not motion.z == 0:
+		look_at(Vector3(-motion.x,0,-motion.z)*SPEED, UP)
 
