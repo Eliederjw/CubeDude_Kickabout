09c4b85044ea9516153e8cf5084f08bc42995f03
Elieder JW <eliederjw@gmail.com>
2020-08-27 19:39:11 -0300

    Lesson 9: Resetting the Game. axis_lock_linear.

--- Level/Level.gd
+++ Level/Level.gd
@@ -1,7 +1,11 @@
 extends Spatial
 
 
-
 func _on_GoalDetector_body_entered(body, goal_id):
 	print ("Player" + str(goal_id) + " has score a goal!")
+	get_tree().call_group("game_pieces", "freeze")
+	$Timer.start()
+
+func _on_Timer_timeout():
+	get_tree().call_group("game_pieces", "reset")
 	
