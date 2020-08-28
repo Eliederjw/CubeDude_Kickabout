3c522f0a700614a8b355eaebc279608164dcd862
Elieder JW <eliederjw@gmail.com>
2020-08-28 17:59:45 -0300

    Lesson 11: Ending the game. reload_current_scene().

--- GUI/GUI.gd
+++ GUI/GUI.gd
@@ -3,3 +3,13 @@ extends CanvasLayer
 func update_score(player, score):
 	var score_label = get_node("Container/Banner/HBoxContainer/Player%sScore" % player)
 	score_label.text = str(score)
+
+func _on_Button_pressed():
+	get_tree().reload_current_scene()
+	
+func game_over(player):
+	var label = $Popup/VBoxContainer/Label
+	label.text = ("Player " + str(player) + "wins!")
+	$Popup.popup_centered()
+
+
--- Level/Level.gd
+++ Level/Level.gd
@@ -3,6 +3,8 @@ extends Spatial
 var Player1_score = 0
 var Player2_score = 0
 
+export var target_score = 3
+
 func _on_GoalDetector_body_entered(body, goal_id):
 	print ("Player" + str(goal_id) + " has score a goal!")
 	get_tree().call_group("game_pieces", "freeze")
@@ -24,3 +26,10 @@ func update_score(player):
 			new_score = Player2_score
 			
 	$GUI.update_score(player, new_score)
+	check_game_over(player, new_score)
+
+func check_game_over(player, score):
+	if score == target_score:
+		$Timer.queue_free()
+		$GUI.game_over(player)
+		
